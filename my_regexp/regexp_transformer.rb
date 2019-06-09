1771628b861498767899bf964cdb634d0b64cca1
pocari <caffelattenonsugar@gmail.com>
2019-06-09 15:39:16 +0900

    transform改善

--- my_regexp/regexp_transformer.rb
+++ my_regexp/regexp_transformer.rb
@@ -5,12 +5,13 @@ module MyRegexp
     rule(char: simple(:x)) { Char.new(x.to_s) }
     rule(escaped: simple(:x)) { Char.new(x.to_s) }
     rule(escaped_char: simple(:x)) { x }
-    rule(left: simple(:x)) { x }
-    rule(right: simple(:x)) { x }
     rule(plus: simple(:x)) { Plus.new(x) }
     rule(star: simple(:x)) { Star.new(x) }
+    rule(left: simple(:x)) { x }
+    rule(right: simple(:x)) { x }
 
     rule(list: sequence(:x)) {
+      # injectは引数なしで、要素数1の場合、x[0]を返すのでいずれにせよNodeオブジェクトが返る
       x.inject do |acc, n|
         List.new(acc, n)
       end
